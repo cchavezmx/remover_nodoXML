255e530098cc2393c48f2fa0ad3ca03275360d64
cchavezmx <cchavezmx@outlook.com>
2021-11-27 05:17:49 -0600

    se añade contador y se cambia el nombre del archivo

--- public/main.js
+++ public/main.js
@@ -3,33 +3,25 @@ const h1 = document.querySelector('h1')
 const btn = document.querySelector('button')
 const input = document.querySelector('input')
 
-
-btn.addEventListener('click', async () => {    
-  // input multiple files map
-  const files = Array.from(input.files)
-  await Promise.all(files.map(file => removeNode(file)))
-
-})
-
-
-input.addEventListener('change', () => {
-  // get data array from input
-  console.log(JSON.stringify(input.files))
+let userAgent
+addEventListener('load', () => {
+  userAgent = window.navigator.userAgent  
 })
 
 const removeNode = async (file) => {
-  // parse xml remove tfd:TimbreFiscalDigital and return file xml 
-  // get data array from input
+  
+  console.log(file)
+
   const reader = new FileReader()
   reader.readAsText(file)
 
-  reader.onload = () => {
-    console.log(reader.result)
+  reader.onload = () => {    
     const parser = new DOMParser()
     const xmlDoc = parser.parseFromString(reader.result, "text/xml")
 
     let xmlString = new XMLSerializer().serializeToString(xmlDoc)
     xmlString = xmlString.replace(/<tfd:TimbreFiscalDigital.*?>/g, '')
+    xmlString = xmlString.replace(/<\/tfd:TimbreFiscalDigital>/g, '')
       
       // download xmlString to xml file
       const blob = new Blob([xmlString], { type: 'text/xml' })
@@ -38,10 +30,28 @@ const removeNode = async (file) => {
       // se crea un element link 
       const a = document.createElement('a')
       a.href = url
-      a.download = `${input.files[0].name}__SIN_NODO.xml`
+      a.download = `${file.name}__SIN_NODO.xml`
       document.body.appendChild(a)
       a.click()
       document.body.removeChild(a)
   
   }
 }
+
+
+btn.addEventListener('click', async () => {    
+  // input multiple files map
+  const files = Array.from(input.files)
+  await Promise.all(files.map(file => removeNode(file)))
+
+  fetch("/insertCount", {
+    body: JSON.stringify({ agent: userAgent, contador: file.files.length }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(res => res)
+
+})
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT
+const fetch = require('node-fetch-commonjs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ extended: true }))
@@ -17,6 +18,37 @@ app.set('view engine', 'ejs')
 // app.use(() => console.log("Hola mundo"))
 app.get("/", function(req, res){
   res.render('index')
+})
+
+const URL = process.env.CONTADOR_URL
+const insertContador = async (agent, contador) => {
+  
+  const insert = new Promise((resolve) => {
+    resolve(
+      fetch(URL, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ agent: agent, contador: contador })
+      })
+    )
+  })
+
+  const response = await insert
+  .then(res => res)
+  return response
+
+}
+
+
+app.post("/insertCount", async({ body }, res) => {  
+  try {
+    const response = await insertContador(body.agent, body.contador)
+    if (response) return res.status(200).json({ message: 'Done' })
+  } catch (error) {
+    
+  }
 })
 
 // para crear un nuevo directorio?
